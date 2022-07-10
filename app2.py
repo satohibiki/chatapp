b500f9534788f62bc198da1d83ee38fcc809f06e
佐藤響 <satohibiki@satoukyounoMacBook-Air.local>
2022-07-10 21:11:51 +0900

    first commit

---
+++ app2.py
@@ -0,0 +1,38 @@
+from datetime import datetime, date
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///chatapp.db"
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(140))
+    due = db.Column(db.String(30))
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template("test.html", posts=posts)
+
+@app.route('/create', methods=("GET", "POST"))
+def create():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        due = datetime.now()
+
+        due = due.strftime("%Y-%m-%d %H:%M")
+
+        new_post = Post(text=text, due=due)
+        
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template("create.html")
+
+if __name__ == '__main__' :
+    app.run(debug=True)
